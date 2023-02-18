dc9d30214a49f561aacc2680e1f473712afe3636
Lye <Lye3@mail.ru>
2023-02-19 02:11:50 +0300

    добавил состояния кнопок фильтра, кнопок карточек каталога

--- source/js/script.js
+++ source/js/script.js
@@ -17,23 +17,22 @@ menuToggle.addEventListener('click', function () {
 });
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-/*   direction: 'vertical', */
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
 });
+
+if (rangeToggle.disabled) {
+  rangeBar.style = "background: #bdbdbd;"
+}
